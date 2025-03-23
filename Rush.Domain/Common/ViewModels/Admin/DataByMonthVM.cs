85ad784fb9de048f7ee735121f7f12dacb781dec
LuisGB20 <luisalbertogomezbroca@gmail.com>
2025-03-23 01:59:46 -0500

    feat: endpoint de admin

---
+++ Rush.Domain/Common/ViewModels/Admin/DataByMonthVM.cs
@@ -0,0 +1,9 @@
+﻿namespace Rush.Domain.Common.ViewModels.Admin
+{
+    public class DataByMonthVM
+    {
+        public int Year { get; set; }
+        public int Month { get; set; }
+        public int dataQuantity { get; set; }
+    }
+}
---
+++ Rush/Controllers/Admin/AdminController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rush.Application.Interfaces.Admin;
+
+namespace Rush.WebAPI.Controllers.Admin
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class AdminController : ControllerBase
+    {
+        private readonly IAdminService _service;
+
+        public AdminController(IAdminService service) 
+        {
+            _service = service;
+        }
+
+        [HttpGet("GetAdminDashboardData")]
+        public async Task<IActionResult> GetAdminDashboardData()
+        {
+            var result = await _service.GetAdminDashboardData();
+            return Ok(result);
+        }
+    }
+}
